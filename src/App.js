bce659f7c706db62a269e7846eddac1812f56126
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-01-29 12:54:20 +1000

    add moviesService and basic state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moviesService from './services/moviesService'
 
 const App = () => {
+  const [moviesList, setMoviesList] = useState([])
+
+  useEffect(() => {
+    const awaitMovieData = async () => {
+      const movieData = await moviesService.getAll()
+      setMoviesList(movieData)
+    }
+    awaitMovieData()
+  }, [])
+
+  console.log(moviesList)
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Hello World!</h1>
+      <ul>
+        {moviesList.map((movie => (
+          <li key={movie.id}>
+            {movie.title}
+          </li>
+        )))}
+      </ul>
+    </div>
   )
 }
 
---
+++ src/services/moviesService.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const baseUrl = 'https://api.themoviedb.org/3/movie'
+const api_key = process.env.REACT_APP_API_KEY
+
+const getAll = async () => {
+  // for fun, we pull a random number of movies from the top_rated endpoint...
+  const maxMovies = 8310
+  const moviesPerPage = 20
+  const nMovies = Math.floor(Math.random() * maxMovies)
+  const nPages = Math.ceil(nMovies / moviesPerPage)
+
+  // get array of top_rated movie ids...
+  let promisesArray = []
+  for (let i = 1; i <= nPages; i++) {
+    promisesArray.push(axios.get(`${baseUrl}/top_rated`, { params: { api_key, page: i } }))
+  }
+  let responses = await axios.all(promisesArray)
+  const idArray = responses.map(response => (
+    response.data.results.map(result => (
+      result.id
+    ))
+  ))
+
+  // use movie ids to get detailed data...
+  promisesArray = idArray.map(id => axios.get(`${baseUrl}/${id}`, { params: { api_key } }))
+  responses = await axios.all(promisesArray)
+
+  const movieArray = responses.map(response => response.data).flat()
+  return movieArray
+}
+
+const moviesService = {
+  getAll,
+}
+
+export default moviesService
