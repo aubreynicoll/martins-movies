3d0eb7edf4b7482dcd715c6086109dac8969f47b
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-01-30 11:45:23 +1000

    place Navbar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
 const Header = () => {
   return (
     <header>
-      <Navbar>
-        
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#"><img src={logo} alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Your</Nav.Link>
+            <Nav.Link href="#">Links</Nav.Link>
+            <NavDropdown title="Here" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Link 1</NavDropdown.Item>
+              <NavDropdown.Item href="#">Link 2</NavDropdown.Item>
+              <NavDropdown.Item href="#">Link 3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>     
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>   
       </Navbar>
     </header>
   )
