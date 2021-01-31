4d096ed5a5d0f7038ff86be6bf62f5f3194ace47
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-01-31 19:31:40 +1000

    update title and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import logo from '../assets/images/logo.png'
 import hero from '../assets/images/hero2.png'
 import Navbar from 'react-bootstrap/Navbar'
