eef41425d668612544ffd4f159a73e296b5ce4de
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-01-31 20:24:39 +1000

    add play button to MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,16 +2,28 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import checkIcon from '../assets/icons/check.svg'
 import starIcon from '../assets/icons/star.png'
+import playIcon from '../assets/icons/play.png'
 
 const MovieCard = ({ movie, isWatched, handleWatchedButtonClick }) => {
   const imdbLink = movie.imdbId ? `https://www.imdb.com/title/${movie.imdbId}/` : null
   const imgSrc = `http://image.tmdb.org/t/p/w342/${movie.image}`
+  const trailerLink = movie.video?.site === 'YouTube'
+    ? `http://www.youtube.com/watch?v=${movie.video.key}`
+    : null
 
   return (
     <div className="MovieCard-root">
       <div className="MovieCard-img-container">
         <img className="MovieCard-img" src={imgSrc} alt="" />
       </div>
+
+      {trailerLink
+        && <a href={trailerLink} target="_blank" rel="noopener noreferrer">
+          <div className="MovieCard-play-button">
+            <img src={playIcon} alt="" />
+          </div>
+        </a>
+      }
 
       <div className="MovieCard-details-container">
         <h3>{movie.title}</h3>
@@ -39,7 +51,7 @@ const MovieCard = ({ movie, isWatched, handleWatchedButtonClick }) => {
         <div className="MovieCard-flex-row">
           <span>
             {imdbLink 
-              && <Button href={imdbLink} target="_blank">Read More</Button>
+              && <Button href={imdbLink} target="_blank" rel="noopener noreferrer">Read More</Button>
             }
           </span>
           <span>
