1499658c35e87e3c570c4297def82f51bac76fe5
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-01 11:52:05 +1000

    remove unused package

--- src/components/TrailerModal.js
+++ src/components/TrailerModal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import Youtube from 'react-youtube'
 
 const TrailerModal = ({ show, onHide, youtubeId }) => {
   return (
