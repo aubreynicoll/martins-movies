1777399ccc9c533e5ccf04e3ac3b5f0c416b3cb5
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-03 10:38:33 +1000

    programmatically get number of pages from API

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -6,33 +6,35 @@ const baseUrl = 'https://api.themoviedb.org/3/movie'
 const api_key = process.env.REACT_APP_API_KEY
 
 const getAll = async () => {
-  // for fun, we pull a random number of movies from the top_rated endpoint...
-  const maxMovies = 8310
-  const moviesPerPage = 20
-  const nMovies = Math.floor(Math.random() * maxMovies)
-  const nPages = Math.ceil(nMovies / moviesPerPage)
+  try {
+    // programmatically get number of pages...
+    const{ data: { total_pages: nPages } } = await axios.get(`${baseUrl}/top_rated`, { params: { api_key } })
 
-  // get array of top_rated movie ids...
-  let promisesArray = []
-  for (let i = 1; i <= nPages; i++) {
-    promisesArray.push(axios.get(`${baseUrl}/top_rated`, { params: { api_key, page: i } }))
+    // get array of top_rated movie ids...
+    let promisesArray = []
+    for (let i = 1; i <= nPages; i++) {
+      promisesArray.push(axios.get(`${baseUrl}/top_rated`, { params: { api_key, page: i } }))
+    }
+    let responsesArray = await axios.all(promisesArray)
+    const idArray = responsesArray.map(response => (
+      response.data.results.map(result => (
+        result.id
+      ))
+    )).flat()
+
+    // enforce rate limiting...
+    const axiosRateLimit = rateLimit(axios.create(), { maxRequests: 200, perMilliseconds: 1 })
+    axiosRateLimit.getMaxRPS()
+
+    // use movie ids to get detailed data...
+    promisesArray = idArray.map(id => axiosRateLimit.get(`${baseUrl}/${id}`, { params: { api_key, append_to_response: 'videos' } }))
+    responsesArray = await axios.all(promisesArray)
+
+    const movieObjArray = responsesArray.map(response => response.data).flat()
+    return movieObjArray.map(movieObj => toMovie(movieObj)) // return formatted data
+  } catch (error) {
+    console.error(error)
   }
-  let responsesArray = await axios.all(promisesArray)
-  const idArray = responsesArray.map(response => (
-    response.data.results.map(result => (
-      result.id
-    ))
-  )).flat()
-
-  const axiosRateLimit = rateLimit(axios.create(), { maxRequests: 200, perMilliseconds: 1 })
-  axiosRateLimit.getMaxRPS()
-
-  // use movie ids to get detailed data...
-  promisesArray = idArray.map(id => axiosRateLimit.get(`${baseUrl}/${id}`, { params: { api_key, append_to_response: 'videos' } }))
-  responsesArray = await axios.all(promisesArray)
-
-  const movieObjArray = responsesArray.map(response => response.data).flat()
-  return movieObjArray.map(movieObj => toMovie(movieObj)) // return formatted data
 }
 
 const moviesService = {
